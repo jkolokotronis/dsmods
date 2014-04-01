7a1def4cc52a39f90486e7cca2ca1ec925b3d854
jkolokotronis <jkolokotronis@live.com>
2014-04-01 09:12:49 +0200

    wah missing objects

--- FallenAngel/scripts/prefabs/arrows.lua
+++ FallenAngel/scripts/prefabs/arrows.lua
@@ -1,7 +1,21 @@
 local assets=
 {
-    Asset("ANIM", "anim/arrows.zip"),
-    Asset("ATLAS", "images/inventoryimages/arrows.xml")
+    Asset("ANIM", "anim/fa_woodarrows.zip"),
+    Asset("ANIM", "anim/fa_icearrows.zip"),
+    Asset("ANIM", "anim/fa_poisonarrows.zip"),
+    Asset("ANIM", "anim/fa_firearrows.zip"),
+    Asset("ANIM", "anim/fa_woodarrows_projectile.zip"),
+    Asset("ANIM", "anim/fa_icearrows_projectile.zip"),
+    Asset("ANIM", "anim/fa_poisonarrows_projectile.zip"),
+    Asset("ANIM", "anim/fa_firearrows_projectile.zip"),
+    Asset("ATLAS", "images/inventoryimages/fa_woodarrows.xml"),
+    Asset("ATLAS", "images/inventoryimages/fa_icearrows.xml"),
+    Asset("ATLAS", "images/inventoryimages/fa_poisonarrows.xml"),
+    Asset("ATLAS", "images/inventoryimages/fa_firearrows.xml"),
+    Asset("IMAGE", "images/inventoryimages/fa_woodarrows.tex"),
+    Asset("IMAGE", "images/inventoryimages/fa_icearrows.tex"),
+    Asset("IMAGE", "images/inventoryimages/fa_poisonarrows.tex"),
+    Asset("IMAGE", "images/inventoryimages/fa_firearrows.tex"),
 }
 
 local function onsewn(inst, target, doer)
@@ -16,34 +30,13 @@ local function fn(Sim)
 
     MakeInventoryPhysics(inst)
     
-    inst.AnimState:SetBank("moditem")
-    inst.AnimState:SetBuild("arrows")
-    inst.AnimState:PlayAnimation("idle")
-    
     inst:AddComponent("inventoryitem")
-    inst.components.inventoryitem.atlasname = "images/inventoryimages/arrows.xml"
 
     inst:AddComponent("inspectable")
     
     inst:AddComponent("stackable")
     inst.components.stackable.maxsize = 99
 
---[[
-    inst:AddComponent("finiteuses")
-    inst.components.finiteuses:SetMaxUses(1)
-    inst.components.finiteuses:SetUses(1)
-    inst.components.finiteuses:SetOnFinished( onfinished )
-    
-    inst:AddComponent("sewing")
-    inst.components.sewing.repair_value = TUNING.SEWINGKIT_REPAIR_VALUE
-    inst.components.sewing.onsewn = onsewn
-    
-    ----------------------------------------------------------------
-
-    ---------------------
-    inst:AddComponent("fuel")
-    inst.components.fuel.fuelvalue = TUNING.SMALL_FUEL
-]]
     inst:AddComponent("reloading")
     inst.components.reloading.returnuses=7
     inst.components.reloading.ammotype="arrows"
@@ -52,13 +45,121 @@ local function fn(Sim)
     inst.components.edible.foodtype = "WOOD"
     inst.components.edible.woodiness = 5
 
-    ---------------------        
     MakeSmallBurnable(inst, TUNING.SMALL_BURNTIME)
     MakeSmallPropagator(inst)
 
-----------------------------------------------------------------
+    return inst
+end
+
+local function woodfn(Sim)
+    local inst=fn(Sim)
+    inst.AnimState:SetBank("fa_woodarrows")
+    inst.AnimState:SetBuild("fa_woodarrows")
+    inst.AnimState:PlayAnimation("idle")
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/fa_woodarrows.xml"
+    inst.components.inventoryitem.imagename="fa_woodarrows"
+    return inst
+end
+
+local function icefn(Sim)
+    local inst=fn(Sim)
+    inst.AnimState:SetBank("fa_icearrows")
+    inst.AnimState:SetBuild("fa_icearrows")
+    inst.AnimState:PlayAnimation("idle")
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/fa_icearrows.xml"
+    inst.components.inventoryitem.imagename="fa_icearrows"
+    return inst
+end
+
+local function firefn(Sim)
+    local inst=fn(Sim)
+    inst.AnimState:SetBank("fa_firearrows")
+    inst.AnimState:SetBuild("fa_firearrows")
+    inst.AnimState:PlayAnimation("idle")
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/fa_firearrows.xml"
+    inst.components.inventoryitem.imagename="fa_firearrows"
+    return inst
+end
+
+local function poisonfn(Sim)
+    local inst=fn(Sim)
+    inst.AnimState:SetBank("fa_poisonarrows")
+    inst.AnimState:SetBuild("fa_poisonarrows")
+    inst.AnimState:PlayAnimation("idle")
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/fa_poisonarrows.xml"
+    inst.components.inventoryitem.imagename="fa_poisonarrows"
+    return inst
+end
+
+local function OnHit(inst, owner, target)
+    inst:Remove()
+end
+
+local function onthrown(inst, data)
+    inst.AnimState:SetOrientation( ANIM_ORIENTATION.OnGround )
+end
+local function projectile()
+    local inst = CreateEntity()
+    local trans = inst.entity:AddTransform()
+    local anim = inst.entity:AddAnimState()
+    MakeInventoryPhysics(inst)
+--    RemovePhysicsColliders(inst)
+    
+    inst:AddTag("projectile")
+    inst.Transform:SetScale(1, 1, 1)
+    
+    inst:AddComponent("projectile")
+    inst:ListenForEvent("onthrown", onthrown)
+    inst.components.projectile:SetSpeed(50)
+    inst.components.projectile:SetOnHitFn(OnHit)
+    inst.components.projectile:SetOnMissFn(OnHit)
     
     return inst
 end
 
-return Prefab( "common/inventory/arrows", fn, assets)
+local function woodprojectile(Sim)
+    local inst = common()
+    inst.AnimState:SetBank("fa_woodarrows_projectile")
+    inst.AnimState:SetBuild("fa_woodarrows_projectile")
+    inst.AnimState:PlayAnimation("idle", true)
+
+    return inst
+end
+
+local function iceprojectile(Sim)
+    local inst = common()
+    inst.AnimState:SetBank("fa_icearrows_projectile")
+    inst.AnimState:SetBuild("fa_icearrows_projectile")
+    inst.AnimState:PlayAnimation("idle", true)
+
+    return inst
+end
+
+local function fireprojectile(Sim)
+    local inst = common()
+    inst.AnimState:SetBank("fa_firearrows_projectile")
+    inst.AnimState:SetBuild("fa_firearrows_projectile")
+    inst.AnimState:PlayAnimation("idle", true)
+
+    return inst
+end
+
+local function poisonprojectile(Sim)
+    local inst = common()
+    inst.AnimState:SetBank("fa_poisonarrows_projectile")
+    inst.AnimState:SetBuild("fa_poisonarrows_projectile")
+    inst.AnimState:PlayAnimation("idle", true)
+
+    return inst
+end
+
+return Prefab( "common/inventory/fa_woodarrows", woodfn, assets),
+ Prefab( "common/inventory/fa_woodarrowsprojectile", woodprojectile, assets),
+  Prefab( "common/inventory/fa_icearrows", icefn, assets),
+   Prefab( "common/inventory/fa_icearrowsprojectile", iceprojectile, assets),
+    Prefab( "common/inventory/fa_poisonarrows", poisonfn, assets),
+    Prefab( "common/inventory/fa_poisonarrowsprojectile", poisonprojectile, assets),
+    Prefab( "common/inventory/fa_firearrows", firefn, assets),
+    Prefab( "common/inventory/fa_firearrowsprojectile", fireprojectile, assets)
+
+
