b9ea0101963906be13f1dfc9d74901f47e0a03f5
kraken121 <kraken121@gmail.com>
2014-04-11 22:50:41 +0800

    fixed rooms

--- Goblin dungeon 1/fa_5x5_goblinroom_5.lua
+++ Goblin dungeon 1/fa_5x5_goblinroom_5.lua
@@ -616,7 +616,7 @@ return {
         },
         {
           name = "",
-          type = "blowdart_sleep",
+          type = "blowdart_pipe",
           shape = "rectangle",
           x = 101,
           y = 227,
