b304b067834c3ec3c35da1b996f1a0aeeae791b5
jkolokotronis <jkolokotronis@live.com>
2014-09-05 00:22:19 +0200

    bypassing the whole mess

--- HeroInTheDark/scripts/prefabs/fa_rings.lua
+++ HeroInTheDark/scripts/prefabs/fa_rings.lua
@@ -16,35 +16,35 @@ local function onfinished(inst)
 end
 
 local function fn(color,type)
-		local inst = CreateEntity()
-		inst.entity:AddTransform()
-		inst.entity:AddAnimState()
-		MakeInventoryPhysics(inst)
-		
+        local inst = CreateEntity()
+        inst.entity:AddTransform()
+        inst.entity:AddAnimState()
+        MakeInventoryPhysics(inst)
+        
     inst.AnimState:SetBank("fa_ring_"..color.."_"..type)
     inst.AnimState:SetBuild("fa_rings")
     inst.AnimState:PlayAnimation("idle")
-	    
-	    local minimap = inst.entity:AddMiniMapEntity()
-    	minimap:SetIcon( "fa_key.tex" )
+        
+        local minimap = inst.entity:AddMiniMapEntity()
+        minimap:SetIcon( "fa_key.tex" )
 
-		inst:AddComponent("inspectable")
-	    inst:AddComponent("inventoryitem")
-    	inst.components.inventoryitem.imagename="fa_ring_"..color.."_"..type
-    	inst.components.inventoryitem.atlasname="images/inventoryimages/fa_rings.xml"
+        inst:AddComponent("inspectable")
+        inst:AddComponent("inventoryitem")
+        inst.components.inventoryitem.imagename="fa_ring_"..color.."_"..type
+        inst.components.inventoryitem.atlasname="images/inventoryimages/fa_rings.xml"
 
 
     inst:AddComponent("equippable")
     inst.components.equippable.equipslot = EQUIPSLOTS.RING
 
-	    
-		inst:AddComponent("tradable")
-	    
-		return inst
-	end
+        
+        inst:AddComponent("tradable")
+        
+        return inst
+    end
 
 local function fnfoli()
-	return fn("green","gold")
+    return fn("green","gold")
 end
 
 
@@ -58,66 +58,88 @@ local function stopfueled(inst, owner)
     if inst.components.fueled then
         inst.components.fueled:StopConsuming()        
     end
+    if(inst.pooptask)then
+        inst.pooptask:Cancel()
+        inst.pooptask=nil
+    end
+end
+
+
+local function startfueledfrozen(inst, owner) 
+    if inst.components.fueled then
+        inst.components.fueled:StartConsuming()        
+    end
+
+    if(inst.pooptask)then
+        inst.pooptask:Cancel()
+        inst.pooptask=nil
+    end
+    inst.pooptask=inst:DoPeriodicTask(1,function()
+            if(owner.components.temperature)then
+                owner.components.temperature.current = math.max( owner.components.temperature.current-1, owner.components.temperature.mintemp)
+            end
+    end)
+end
+
+
+local function startfueledburning(inst, owner) 
+    if inst.components.fueled then
+        inst.components.fueled:StartConsuming()        
+    end
+
+    if(inst.pooptask)then
+        inst.pooptask:Cancel()
+        inst.pooptask=nil
+    end
+   
+    inst.pooptask=inst:DoPeriodicTask(1,function()
+            if(owner.components.temperature)then
+                owner.components.temperature.current =  math.min( owner.components.temperature.current+1, owner.components.temperature.maxtemp)
+            end
+    end)
 end
 
 local function fnfrozen()
-	local inst=fn("blue","silver")
-	if(FA_DLCACCESS)then
-		inst.components.equippable.dapperness =FROZEN_DAPPERNESS
-	else
-		inst:AddComponent("dapperness")
-		inst.components.dapperness.dapperness =FROZEN_DAPPERNESS    
-	end
+    local inst=fn("blue","silver")
+    if(FA_DLCACCESS)then
+        inst.components.equippable.dapperness =FROZEN_DAPPERNESS
+    else
+        inst:AddComponent("dapperness")
+        inst.components.dapperness.dapperness =FROZEN_DAPPERNESS    
+    end
 
-        inst.components.equippable:SetOnEquip( startfueled )
+        inst.components.equippable:SetOnEquip( startfueledfrozen )
         inst.components.equippable:SetOnUnequip( stopfueled )
-        inst:AddComponent("heater")
-        inst.components.heater.iscooler = true
-        --i want it to decline x/sec, so it should return always xc lower than current i guess, although it is still subject to summer/winter insulation
-        inst.components.heater.equippedheatfn=function(owner, observer)
-        	if(owner and owner.components.temperature and owner.components.temperature.current)then
-        		return owner.component.temperature.current-1
-        	else
-        		return 0
-        	end
-	    end
+       
 
         inst:AddComponent("fueled")
         inst.components.fueled.fueltype = "MAGIC"
         inst.components.fueled:InitializeFuelLevel(RING_FUELLEVEL)
         inst.components.fueled:SetDepletedFn(onfinished)
-	return inst
+    return inst
 end
 
 local function fnburning()
-	local inst=fn("red","bronze")
-	if(FA_DLCACCESS)then
-		inst.components.equippable.dapperness =BURNING_DAPPERNESS
-	else
-		inst:AddComponent("dapperness")
-		inst.components.dapperness.dapperness =BURNING_DAPPERNESS    
-	end
+    local inst=fn("red","bronze")
+    if(FA_DLCACCESS)then
+        inst.components.equippable.dapperness =BURNING_DAPPERNESS
+    else
+        inst:AddComponent("dapperness")
+        inst.components.dapperness.dapperness =BURNING_DAPPERNESS    
+    end
 
-        inst.components.equippable:SetOnEquip( startfueled )
+        inst.components.equippable:SetOnEquip( startfueledburning )
         inst.components.equippable:SetOnUnequip( stopfueled )
-    inst:AddComponent("heater")
-	inst.components.heater.equippedheatfn=function(owner, observer)
-        	if(owner and owner.components.temperature and owner.components.temperature.current)then
-        		return owner.component.temperature.current+1
-        	else
-        		return 0
-        	end
-	    end
 
         inst:AddComponent("fueled")
         inst.components.fueled.fueltype = "MAGIC"
         inst.components.fueled:InitializeFuelLevel(RING_FUELLEVEL)
         inst.components.fueled:SetDepletedFn(onfinished)
-	return inst
+    return inst
 end
 
 local function fnspeed()
-	local inst=fn("green","silver")
+    local inst=fn("green","silver")
     inst.components.equippable.walkspeedmult = SPEED_MULT
         inst.components.equippable:SetOnEquip( startfueled )
         inst.components.equippable:SetOnUnequip( stopfueled )
@@ -125,7 +147,7 @@ local function fnspeed()
         inst.components.fueled.fueltype = "MAGIC"
         inst.components.fueled:InitializeFuelLevel(RING_FUELLEVEL)
         inst.components.fueled:SetDepletedFn(onfinished)
-	return inst
+    return inst
 end
 
 
@@ -173,7 +195,7 @@ local function stoplight(inst, owner)
 end
 
 local function fnlight()
-	local inst=fn("yellow","gold")
+    local inst=fn("yellow","gold")
 
     local light = inst.entity:AddLight()
     light:SetFalloff(0.8)
@@ -186,55 +208,55 @@ local function fnlight()
         inst.components.equippable:SetOnEquip( startlight )
         inst.components.equippable:SetOnUnequip( stoplight )
 
-	if(FA_DLCACCESS)then
-		inst.components.equippable.dapperness =LIGHT_DAPPERNESS
-	else
-		inst:AddComponent("dapperness")
-		inst.components.dapperness.dapperness =LIGHT_DAPPERNESS    
-	end
+    if(FA_DLCACCESS)then
+        inst.components.equippable.dapperness =LIGHT_DAPPERNESS
+    else
+        inst:AddComponent("dapperness")
+        inst.components.dapperness.dapperness =LIGHT_DAPPERNESS    
+    end
 
         inst:AddComponent("fueled")
         inst.components.fueled.fueltype = "MAGIC"
         inst.components.fueled:InitializeFuelLevel(RING_FUELLEVEL)
         inst.components.fueled:SetDepletedFn(onfinished)
-	return inst
+    return inst
 end
 
 
 local function startpoop(inst, owner) 
-	if(inst.pooptask)then
-		inst.pooptask:Cancel()
-		inst.pooptask=nil
-	end
-	inst.pooptask=inst:DoPeriodicTask(2,function()
-		if(owner and owner:IsValid() and not (owner.components.health and owner.components.health:IsDead()))then
-			local poo = SpawnPrefab("poop")
-	        poo.Transform:SetPosition(owner.Transform:GetWorldPosition())  
+    if(inst.pooptask)then
+        inst.pooptask:Cancel()
+        inst.pooptask=nil
+    end
+    inst.pooptask=inst:DoPeriodicTask(2,function()
+        if(owner and owner:IsValid() and not (owner.components.health and owner.components.health:IsDead()))then
+            local poo = SpawnPrefab("poop")
+            poo.Transform:SetPosition(owner.Transform:GetWorldPosition())  
             --simply making a hunger_drain comp might be better... cant just change 'rate' because changing that would get overwritten by various other factors
             if(owner.components.hunger)then
                 owner.components.hunger:DoDelta(-owner.components.hunger.hungerrate*2)
             end
-		else
-			print("warning: invalid owner of equipped ring!")
-		end
-	end)
+        else
+            print("warning: invalid owner of equipped ring!")
+        end
+    end)
     if inst.components.fueled then
         inst.components.fueled:StartConsuming()        
     end
 end
 
 local function stoppoop(inst, owner) 
-	if(inst.pooptask)then
-		inst.pooptask:Cancel()
-		inst.pooptask=nil
-	end
+    if(inst.pooptask)then
+        inst.pooptask:Cancel()
+        inst.pooptask=nil
+    end
     if inst.components.fueled then
         inst.components.fueled:StopConsuming()        
     end
 end
 
 local function fnpoop()
-	local inst=fn("orange","bronze")
+    local inst=fn("orange","bronze")
         inst.components.equippable:SetOnEquip( startpoop )
         inst.components.equippable:SetOnUnequip( stoppoop )
 
@@ -242,10 +264,10 @@ local function fnpoop()
         inst.components.fueled.fueltype = "MAGIC"
         inst.components.fueled:InitializeFuelLevel(RING_FUELLEVEL)
         inst.components.fueled:SetDepletedFn(onfinished)
-	return inst
+    return inst
 end 
 
-return	Prefab( "common/inventory/fa_ring_green_gold", fnfoli, assets),
+return  Prefab( "common/inventory/fa_ring_green_gold", fnfoli, assets),
 Prefab( "common/inventory/fa_ring_frozen", fnfrozen, assets),
 Prefab( "common/inventory/fa_ring_burning", fnburning, assets),
 Prefab( "common/inventory/fa_ring_speed", fnspeed, assets),
